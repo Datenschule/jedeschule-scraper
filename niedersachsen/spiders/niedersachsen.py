eb4c3fd9e2803abf5e1a35ed079809fc4e074526
Knut Hühne <knut@k-nut.eu>
2016-11-23 20:26:36 +0100

    initial commit

---
+++ niedersachsen/spiders/niedersachsen.py
@@ -0,0 +1,34 @@
+
+import scrapy
+
+def cleanjoin(listlike):
+    """ returns string of joined items in list,
+        removing whitespace """
+    return "".join([text.strip() for text in listlike])
+
+class NiedersachsenSpider(scrapy.Spider):
+    name = 'niedersachsenspider'
+    start_urls = ['http://schulnetz.nibis.de/db/schulen/suche_2.php']
+
+    def parse(self, response):
+        for link in response.css('tr.fliess td:nth-child(4) a ::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(link), callback=self.parse_detail)
+
+        for link in response.css('tr.fliessbgg td:nth-child(4) a ::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(link), callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        collection = {}
+        for index, row in enumerate(response.css('tr')):
+            # skip disclaimer header
+            if index == 0:
+                continue
+            tds = row.css('td')
+
+            # last character is ":". Strip that
+            row_key = cleanjoin(tds[0].css('::text').extract())[:-1]
+            row_value = cleanjoin(tds[1].css('::text').extract())
+            collection[row_key] = row_value
+        yield collection
+
+        
---
+++ niedersachsen/spiders/schleswig_holstein.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.shell import inspect_response
+
+
+class SchleswigHolsteinSpider(scrapy.Spider):
+    name = "schleswig-holstein"
+    start_urls = ['http://schulportraets.schleswig-holstein.de/portal/schule_suchen//']
+
+    def parse(self, response):
+        yield scrapy.FormRequest.from_response(
+                response,
+                callback=self.after_login
+                )
+
+    def after_login(self, response):
+        links = response.css("table tbody td b a::attr(href)").extract()
+        for link in links:
+            # turns /20/ into /20-1/ which is the details page that contains the data
+            detail_link = link[:-1] + "-1/"
+            yield scrapy.Request(response.urljoin(detail_link), callback=self.parse_detail)
+        yield scrapy.FormRequest.from_response(
+                response,
+                formcss=".userInput",
+                callback=self.other_pages
+                )
+
+    def other_pages(self, response):
+        print("_______>", response.request.body)
+        links = response.css("table tbody td b a::attr(href)").extract()
+        for link in links:
+            # turns /20/ into /20-1/ which is the details page that contains the data
+            detail_link = link[:-1] + "-1/"
+            yield scrapy.Request(response.urljoin(detail_link), callback=self.parse_detail)
+        yield scrapy.FormRequest.from_response(
+                response,
+                formcss="#content > div > form:nth-child(6)",
+                callback=self.other_pages
+                )
+
+    def parse_detail(self, response):
+        print(response.url)
+
+        collection = {}
+        # inspect_response(response, self)
+        for row in response.css('tbody tr'):
+            tds = row.css('td')
+
+            # last character is ":". Strip that
+            row_key = tds[0].css('::text').extract_first().strip()
+            row_value = tds[1].css('::text').extract_first().strip()
+            collection[row_key] = row_value
+        yield collection
+
+

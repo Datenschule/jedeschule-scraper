7a0bedef41a1becad5f66a41cf50e31210f00067
Knut Hühne <knut@k-nut.eu>
2016-11-25 16:37:24 +0100

    Add Bremen spider

---
+++ jedeschule/spiders/bremen.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.shell import inspect_response
+
+
+class BremenSpider(scrapy.Spider):
+    name = "bremen"
+    start_urls = ['http://www.bildung.bremen.de/detail.php?template=35_schulsuche_stufe2_d']
+
+    def parse(self, response):
+        for link in response.css(".table_daten_container a ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(link), callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        lis = response.css(".kogis_main_visitenkarte ul li")
+
+        collection = {}
+        collection['name'] = response.css(".main_article h3 ::text").extract_first()
+        for li in lis:
+            key = li.css("span ::attr(title)").extract_first()
+            value = " ".join([part.strip() for part in li.css("::text").extract()])
+            # Filter out this pointless entry
+            if value != "Standortanzeige":
+                collection[key] = value
+        yield collection

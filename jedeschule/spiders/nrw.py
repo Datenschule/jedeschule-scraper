a3797089b4280b01fe68286902afd33de39cbfda
Knut Perseke <knut.perseke@gmail.com>
2016-11-25 21:30:29 +0100

    add scraper for nrw

---
+++ jedeschule/spiders/nrw.py
@@ -0,0 +1,33 @@
+import scrapy
+from scrapy.shell import inspect_response
+
+class SachsenSpider(scrapy.Spider):
+    name = "nrw"
+    base_url = "https://www.schulministerium.nrw.de/BiPo/SchuleSuchen/"
+
+    start_urls = ['https://www.schulministerium.nrw.de/BiPo/SchuleSuchen/online']
+
+    def parse(self, response):
+        url = response.css(".bp_tab tr")[4].css("td")[1].css('a::attr(href)').extract_first().strip()
+        return scrapy.Request(self.base_url + url, callback=self.parse_search)
+
+    def parse_search(self, response):
+        return scrapy.FormRequest.from_response(response, callback=self.parse_schoollist)
+
+    def parse_schoollist(self, response):
+        for tr in response.css('table tr'):
+            collection = {}
+            collection['Schulform'] = tr.css('td::text').extract()[1].strip()
+            url = tr.css('td')[3].css('a::attr(href)').extract_first().strip()
+            request = scrapy.Request(self.base_url + url, callback=self.parse_overview)
+            request.meta['collection'] = collection
+            yield request
+
+    def parse_overview(self, response):
+        collection = response.meta['collection']
+        for tr in response.css("table tr"):
+            tds = tr.css("td ::text").extract()
+            # sometimes there is no value for the key
+            if len(tds[0].strip()) > 0 and len(tds) == 2:
+                collection[tds[0].strip()] = tds[1].strip()
+        yield collection

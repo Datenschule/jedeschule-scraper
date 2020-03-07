cb51fbf5ed3eed3c61f286b2031a2bd33b89f32f
Knut Perseke <knut.perseke@okfn.de>
2020-03-07 15:43:06 +0100

    merges bremen spider

--- jedeschule/spiders/bremen.py
+++ jedeschule/spiders/bremen.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy import Item
-from scrapy.exceptions import DropItem
-from scrapy.shell import inspect_response
 
 from jedeschule.items import School
 from jedeschule.spiders.school_spider import SchoolSpider
@@ -13,45 +12,46 @@ class BremenSpider(SchoolSpider):
     start_urls = ['http://www.bildung.bremen.de/detail.php?template=35_schulsuche_stufe2_d']
 
     def parse(self, response):
-        for link in response.css(".table_erklaerung a"):
-            detail_url = link.css("::attr(href)").get()
-            school_id = link.css("em ::text").get()
-
-            yield scrapy.Request(response.urljoin(detail_url), callback=self.parse_detail, meta={'id': school_id})
+        for link in response.css(".table_daten_container a ::attr(href)").extract():
+            request = scrapy.Request(response.urljoin(link), callback=self.parse_detail)
+            request.meta['id'] = link.split("de&Sid=", 1)[1]
+            yield request
 
     def parse_detail(self, response):
         lis = response.css(".kogis_main_visitenkarte ul li")
-
-        item = {}
-
-        item['id'] = response.meta['id']
-        item['data_url'] = response.url
-
-        if len(lis) == 0:
-            # Detail page contains no info, see https://github.com/Datenschule/jedeschule-scraper/issues/54
-            raise DropItem(item)
-
-        item['name'] = response.css(".main_article h3 ::text").extract_first()
+        collection = {}
+        collection['id'] = response.meta['id'].zfill(3)
+        collection['name'] = response.css(".main_article h3 ::text").extract_first()
         for li in lis:
             key = li.css("span ::attr(title)").extract_first()
             value = " ".join([part.strip() for part in li.css("::text").extract()])
             # Filter out this pointless entry
             if key is not None:
-                item[key] = value
+                collection[key] = value
+            collection['data_url'] = response.url
+        yield collection
 
-        yield item
+    def fix_number(number):
+        new = ''
+        for letter in number:
+            if letter.isdigit():
+                new+=letter
+        return new
 
     @staticmethod
     def normalize(item: Item) -> School:
         ansprechpersonen = item['Ansprechperson'].replace('Schulleitung:', '').replace('Vertretung:', ',').split(',')
         item['Schulleitung'] = ansprechpersonen[0]
         item['Vertretung'] = ansprechpersonen[1]
-
-        return School(  id=item.get('id'),
-                        name=item.get('name'),
-                        address=item.get('Anschrift:'),
+        return School(name=item.get('name'),
+                        id='HB-{}'.format(item.get('id')),
+                        address=re.split('\d{5}', item.get('Anschrift:').strip())[0].strip(),
+                        zip=re.findall('\d{5}', item.get('Anschrift:').strip())[0],
+                        city=re.split('\d{5}', item.get('Anschrift:').strip())[1].strip(),
                         website=item.get('Internet'),
-                        email=item.get('E-Mail-Adresse'),
-                        fax=item.get('Telefax'),
-                        phone=item.get('Telefon'),
-                        director=item.get('Schulleitung'))
+                        email=item.get('E-Mail-Adresse').strip(),
+                        fax=BremenSpider.fix_number(item.get('Telefax')),
+                        phone=BremenSpider.fix_number(item.get('Telefon'))
+                        )
+    
+

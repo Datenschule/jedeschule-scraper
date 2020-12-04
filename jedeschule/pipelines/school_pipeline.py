6a490b54e292f65d30d05925a67472f385dd2a09
htw-kevkev <67795307+htw-kevkev@users.noreply.github.com>
2020-12-04 18:43:41 +0100

    changed pipeline from database to json

--- jedeschule/pipelines/school_pipeline.py
+++ jedeschule/pipelines/school_pipeline.py
@@ -1,18 +1,23 @@
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
-from scrapy import Item
+# from scrapy import Item
 
-from jedeschule.items import School
+# from jedeschule.items import School
 from jedeschule.spiders.school_spider import SchoolSpider
 
 
-@dataclass
-class SchoolPipelineItem:
-    info: School
-    item: Item
+# @dataclass
+# class SchoolPipelineItem:
+#     info: School
+#     item: Item
 
+
+# class SchoolPipeline(object):
+#     def process_item(self, item, spider: SchoolSpider) -> SchoolPipelineItem:
+#         school = spider.normalize(item)
+#         return SchoolPipelineItem(info=school, item=item)
 
 class SchoolPipeline(object):
-    def process_item(self, item, spider: SchoolSpider) -> SchoolPipelineItem:
+    def process_item(self, item, spider: SchoolSpider):
         school = spider.normalize(item)
-        return SchoolPipelineItem(info=school, item=item)
+        return {'info': school, 'item': item}

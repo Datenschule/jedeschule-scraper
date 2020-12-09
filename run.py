f8447c253ca825546cb01532ed2ce22ced9e7b38
htw-kevkev <67795307+htw-kevkev@users.noreply.github.com>
2020-12-09 14:00:46 +0100

    Run MV as spider

--- run.py
+++ run.py
@@ -27,93 +27,11 @@ from jedeschule.spiders.thueringen import ThueringenSpider
 from jedeschule.spiders.schleswig_holstein import SchleswigHolsteinSpider
 from jedeschule.spiders.berlin import BerlinSpider
 from jedeschule.spiders.rheinland_pfalz import RheinlandPfalzSpider
+from jedeschule.spiders.mecklenburg_vorpommern import MecklenburgVorpommernSpider
 
 configure_logging()
 settings = get_project_settings()
 runner = CrawlerRunner(settings)
-
-
-def get_mv():
-    filename = 'mv.xlsx'
-    url_mv = 'http://service.mvnet.de/_php/download.php?datei_id=1619185'
-    wget.download(url_mv, filename)
-    workbook = xlrd.open_workbook(filename)
-    sheets = ['Schulverzeichnis öffentl. ABS', 'Schulverzeichnis öffentl. BLS','Schulverzeichnis freie ABS']
-
-    legend = {
-        'schulart': {
-            'Agy': 'Abendgymnasium',
-            'FöL': 'Schule mit dem Förderschwerpunkt Lernen',
-            'FöS': 'Schule mit dem Förderschwerpunkt Sehen',
-            'FöSp': 'Schule mit dem Förderschwerpunkt Sprache',
-            'FöK': 'Schule mit dem Förderschwerpunkt körperliche und motorische Entwicklung',
-            'FöK/GS': 'Schule mit dem Förderschwerpunkt körperliche und motorische Entwicklung mit Grundschule',
-            'FöG': 'Schule mit dem Förderschwerpunkt geistige Entwicklung',
-            'FöKr': 'Schule mit dem Förderschwerpunkt Unterricht kranker Schülerinnen und Schüler',
-            'FöL/FöG': 'Schule mit dem Förderschwerpunkt Lernen und  dem Förderschwerpunkt geistige Entwicklung',
-            'FöV': 'Schule mit dem Förderschwerpunkt emotionale und soziale Entwicklung',
-            'FöV/FöKr': 'Schule mit dem Förderschwerpunkt emotionale und soziale Entwicklung und dem Förderschwerpunkt Unterricht kranker Schülerinnen und Schüler',
-            'FöV/FöL': 'Schule mit dem Förderschwerpunkt emotionale und soziale Entwicklung und dem Förderschwerpunkt Lernen)',
-            'FöL/FöV/FöKr': 'Schule mit den Förderschwerpunkten Lernen, dem Förderschwerpunkt emotionale und soziale Entwicklung sowie dem Förderschwerpunkt Unterricht kranker Schülerinnen und Schüler',
-            'FöH': 'Schule mit dem Förderschwerpunkt Hören',
-            'GS': 'Grundschule',
-            'GS/OS': 'Grundschule mit schulartunabhängiger Orientierungsstufe',
-            'GS/FöSp': 'Grundschule mit selbstständigen Klassen mit dem Förderschwerpunkt Sprache',
-            'GS/OS/Gy': 'Grundschule mit schulartunabhängiger Orientierungsstufe und Gymnasium',
-            'Gy': 'Gymnasium',
-            'Gy/GS/OS': 'Gymnasium mit Grundschule und schulartunabhängiger Orientierungsstufe',
-            'Gy/RegS/GS': 'Gymnasium mit Regionaler Schule und Grundschule',
-            'IGS': 'Integrierte Gesamtschule',
-            'IGS/GS': 'Integrierte Gesamtschule mit Grundschule',
-            'IGS/GS/FöG': 'Integrierte Gesamtschule mit Grundschule  und Schule mit dem Förderschwerpunkt geistige Entwicklung',
-            'KGS': 'Kooperative Gesamtschule',
-            'KGS/GS': 'Kooperative Gesamtschule mit Grundschule',
-            'KGS/GS/\nFöL': 'Kooperative Gesamtschule mit Grundschule und Schule mit dem Förderschwerpunkt Lernen',
-            'RegS': 'Regionale Schule',
-            'RegS/GS': 'Regionale Schule mit Grundschule',
-            'RegS/Gy': 'Regionale Schule mit Gymnasium',
-            'WS': 'Waldorfschule'
-        },
-        'schulamt': {
-            'GW': 'Greifswald',
-            'NB': 'Neubrandenburg',
-            'RO': 'Rostock',
-            'SN': 'Schwerin'
-        },
-        'landkreis': {
-            'HRO': 'Hansestadt Rostock',
-            'SN': 'Landeshauptstadt Schwerin',
-            'LRO': 'Landkreis Rostock',
-            'LUP': 'Landkreis Ludwigslust-Parchim',
-            'MSE': 'Landkreis Mecklenburgische Seenplatte',
-            'NWM': 'Landkreis Nordwestmecklenburg',
-            'VG': 'Landkreis Vorpommern-Greifswald',
-            'VR': 'Landkreis Vorpommern-Rügen'
-        }
-    }
-    data = []
-    for sheet in sheets:
-        worksheet = workbook.sheet_by_name(sheet)
-        keys = [v.value for v in worksheet.row(0)]
-        for row_number in range(worksheet.nrows):
-            if row_number == 0:
-                continue
-            row_data = {}
-            for col_number, cell in enumerate(worksheet.row(row_number)):
-                row_data[keys[col_number]] = cell.value
-            #if (row_data['Schulname'] != ''):
-            #    row_data['Staatl. Schulamt'] = legend['schulamt'][row_data['Staatl. Schulamt']]
-            #    row_data['Landkreis/ kreisfr. Stadt'] = legend['landkreis'][row_data['Landkreis/ kreisfr. Stadt']]
-            #    if sheet != 'Schulverzeichnis öffentl. BLS':
-            #        row_data['Schulart/ Org.form'] = legend['schulart'][row_data['Schulart/ Org.form']]
-            #    else:
-            #        row_data['Schulart/ Org.form'] = 'Berufliche Schule'
-            # TODO: Do all the cleaning in the school pipeline
-            data.append(row_data)
-
-    with open('data/mecklenburg-vorpommern.json', 'w') as json_file:
-        json_file.write(json.dumps(data))
-    os.remove(filename)
 
 
 def __retrieve_keys(url):
@@ -202,11 +120,11 @@ def crawl():
     yield runner.crawl(SchleswigHolsteinSpider)
     yield runner.crawl(BerlinSpider)
     yield runner.crawl(RheinlandPfalzSpider)
+    yield runner.crawl(MecklenburgVorpommernSpider)
     reactor.stop()
 
 
 if __name__ == '__main__':
     crawl()
     reactor.run()  # the script will block here until the last crawl call is finished
-    get_mv()
     get_nrw()

decebe028d23381d070d9b7845fcd81cce72b4f9
htw-kevkev <67795307+htw-kevkev@users.noreply.github.com>
2021-01-21 19:02:00 +0100

    Updated Berlin scraper (#81)
    * changed pipeline from database to json
    
    * Revert "changed pipeline from database to json"
    
    * Fixed base url and removed unnecessary methods which scraped subpages
    
    * Removed code which was commented out
    
    * parse school name and ID correctly
    
    Co-authored-by: Matthias Jacob <dev@matthiasjacob.net>

--- jedeschule/spiders/berlin.py
+++ jedeschule/spiders/berlin.py
@@ -1,102 +1,84 @@
 # -*- coding: utf-8 -*-
+import urllib.parse as urlparse
+from typing import List
+from urllib.parse import parse_qs
 import scrapy
 from scrapy.shell import inspect_response
+from jedeschule.items import School
+from scrapy import Item
 import re
 
 
 class BerlinSpider(scrapy.Spider):
     name = "berlin"
-    # allowed_domains = ["www.berlin.de/sen/bildung/schule/berliner-schulen/schulverzeichnis/SchulListe.aspx"]
-    base_url = 'http://www.berlin.de/sen/bildung/schule/berliner-schulen/schulverzeichnis/'
-    start_url = base_url + 'SchulListe.aspx/'
+    # Potential errors of Berlin:
+    # 502 with user agent = default (scrapy) -> use a real user agent like "jedeschule"
+    # 429 with download delay = default -> set download delay to slow down scrapy
+    # custom settings avoid other spiders from being affected of solving a spider individual problem
+    custom_settings = {'USER_AGENT': 'jedeschule (open data project)', 'DOWNLOAD_DELAY': 1,}
+    base_url = 'https://www.berlin.de/sen/bildung/schule/berliner-schulen/schulverzeichnis/'
+    start_url = base_url + 'SchulListe.aspx'
     start_urls = [start_url]
 
     def parse(self, response):
         schools = response.css('td a::attr(href)').extract()
         for i, school in enumerate(schools):
-            yield scrapy.Request(self.start_url + school, callback=self.parse_detail, meta={'cookiejar': i})
-
+            # school = school.replace(' ','')
+            yield scrapy.Request(self.base_url + school, callback=self.parse_detail, meta={'cookiejar': i})
 
     def parse_detail(self, response):
         meta = {}
-        name = response.css('#ContentPlaceHolderMenuListe_lblSchulname::text').extract_first().strip().rsplit('-', 1)
-        meta['name'] = name[0]
-        meta['id'] = name[1]
+        name = response.css('#ContentPlaceHolderMenuListe_lblSchulname::text').extract_first().strip()#.rsplit('-', 1)
+        meta['name'] = self.fix_data(name)
+        meta['id'] = self._parse_school_no(response.url)
+        meta['address'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblStrasse::text').extract_first())
+        meta['zip'], meta['city'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblOrt::text').extract_first()).split(" ", 1)
         schooltype = re.split('[()]', response.css('#ContentPlaceHolderMenuListe_lblSchulart::text').extract_first())
-        meta['schooltype'] = schooltype[0].strip()
-        meta['legal_status'] = schooltype[1].strip()
-        meta['telephone'] = response.css('#ContentPlaceHolderMenuListe_lblTelefon::text').extract_first()
-        meta['fax'] = response.css('#ContentPlaceHolderMenuListe_lblFax::text').extract_first()
-        meta['mail'] = response.css('#ContentPlaceHolderMenuListe_HLinkEMail::text').extract_first()
-        meta['web'] = response.css('#ContentPlaceHolderMenuListe_HLinkWeb::attr(href)').extract_first()
-        headmaster = response.css('#ContentPlaceHolderMenuListe_lblLeitung::text').extract_first().strip()
-        meta['headmaster'] = ' '.join(headmaster.split(',')[::-1]).strip()
+        meta['schooltype'] = self.fix_data(schooltype[0].strip())
+        meta['legal_status'] = self.fix_data(schooltype[1].strip())
+        meta['telephone'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblTelefon::text').extract_first())
+        meta['fax'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblFax::text').extract_first())
+        meta['mail'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_HLinkEMail::text').extract_first())
+        meta['web'] = self.fix_data(response.css('#ContentPlaceHolderMenuListe_HLinkWeb::attr(href)').extract_first())
+        headmaster = response.css('#ContentPlaceHolderMenuListe_lblLeitung::text').extract_first()
+        if headmaster:
+            meta['headmaster'] = self.fix_data(' '.join(headmaster.split(',')[::-1]).strip())  
         meta['cookiejar'] = response.meta['cookiejar']
-        activities = response.css('#ContentPlaceHolderMenuListe_lblAGs::text').extract_first()
+        meta['data_url'] = response.url
+        activities = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblAGs::text').extract_first())
         if activities:
             meta['activities'] = [x.strip() for x in activities.split(';')]
-        partner = response.css('#ContentPlaceHolderMenuListe_lblPartner::text').extract_first()
+        partner = self.fix_data(response.css('#ContentPlaceHolderMenuListe_lblPartner::text').extract_first())
         if partner:
             meta['partner'] = [x.strip() for x in partner.split(';')]
-        yield scrapy.Request('https://www.berlin.de/sen/bildung/schule/berliner-schulen/schulverzeichnis/schuelerschaft.aspx?view=jgs',
-                             meta=meta, callback=self.call_students, dont_filter=True)
+        yield meta
 
-    def call_students(self, response):
-        links = response.css('#NaviSchuelerschaft ul ul li a::attr(href)').extract()
-        meta = response.meta
-        meta['cookiejar'] = response.meta['cookiejar']
-        if len(links) > 0:
-            meta['student_links'] = links[1:]
+    def _parse_school_no(self, url):
+        """Parses the school number from the 'IDSchulzweig' parameter in the url"""
+        parsed = urlparse.urlparse(url)
+        id_in_url: List[str] = parse_qs(parsed.query, max_num_fields=1)['IDSchulzweig']
+        assert len(id_in_url) == 1
 
-            yield scrapy.Request(self.base_url + links[0], meta=response.meta, dont_filter=True, callback=self.parse_students)
-        else:
-            yield response.meta
+        return id_in_url[0].strip()
 
-    def parse_students(self, response):
-        # inspect_response(response, self)
-        year = response.request.url.rsplit('=', 1)[1]
-        meta = response.meta
-        if 'students' not in meta.keys():
-            meta['students'] = {}
-        meta['students'][year] = self.parse_table(response.css('#ContentPlaceHolderMenuListe_GridViewJahrgansstufen'))
-        links = meta['student_links']
+    # fix wrong tabs, spaces and new lines
+    def fix_data(self, string):
+        if string:
+            string = ' '.join(string.split())
+            string.replace('\n', '')
+            string.replace('\t', '')
+        return string
 
-        if len(links) > 0:
-            meta['student_links'] = links[1:]
-            yield scrapy.Request(self.base_url + links[0], meta=response.meta, dont_filter=True, callback=self.parse_students)
-        else:
-            yield scrapy.Request('https://www.berlin.de/sen/bildung/schule/berliner-schulen/schulverzeichnis/schulpersonal.aspx?view=pers',
-                                 meta=meta, callback=self.call_teacher, dont_filter=True)
-
-    def call_teacher(self, response):
-        links = response.css('#NaviSchulpersonal ul ul li a::attr(href)').extract()
-        meta = response.meta
-        meta['cookiejar'] = response.meta['cookiejar']
-        if len(links) > 0:
-            meta['teacher_links'] = links[1:]
-            yield scrapy.Request(self.base_url + links[0], meta=response.meta, dont_filter=True, callback=self.parse_teachers)
-        else:
-            yield response.meta
-
-    def parse_teachers(self, response):
-        year = response.request.url.rsplit('=', 1)[1]
-        meta = response.meta
-        if 'teachers' not in meta.keys():
-            meta['teachers'] = {}
-        meta['teachers'][year] = self.parse_table(response.css('#ContentPlaceHolderMenuListe_GridViewPersonal'))
-        links = meta['teacher_links']
-
-        if len(links) > 0:
-            meta['teacher_links'] = links[1:]
-            yield scrapy.Request(self.base_url + links[0], meta=meta, dont_filter=True, callback=self.parse_students)
-        else:
-            # inspect_response(response, self)
-            yield meta
-
-    def parse_table(self, table):
-        result = {}
-        keys = table.css('th::text').extract()
-        for row in table.css('tr'):
-            for idx, value in enumerate(row.css('td')):
-                result[keys[idx]] = value.css('::text').extract_first()
-        return result
+    def normalize(self, item: Item) -> School:
+        return School(name=item.get('name'),
+                      id='BE-{}'.format(item.get('id')),
+                      address=item.get('address'),
+                      zip=item.get('zip'),
+                      city=item.get('city'),
+                      website=item.get('web'),
+                      email=item.get('mail'),
+                      school_type=item.get('schooltype'),
+                      fax=item.get('fax'),
+                      phone=item.get('telephone'),
+                      director=item.get('headmaster'),
+                      legal_status=item.get('legal_status'))

6db7acab3ab8f97e36472f05686168c7d4de8611
Knut Hühne <knut@k-nut.eu>
2021-01-23 19:00:40 +0100

    Use plain scraps selectors, not lxml

--- jedeschule/spiders/saarland.py
+++ jedeschule/spiders/saarland.py
@@ -1,55 +1,50 @@
-# -*- coding: utf-8 -*-
-import copy
-from scrapy import Item
-from jedeschule.items import School
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
-from lxml import html
+from scrapy import Item
+
+from jedeschule.items import School
 
 # School types: Berufliche Schule, Erweitere Realschule, Förderschule, Freie Waldorfschule,
 # Gemeinschatsschule, Grundschule, Gymnasium, Lyzeum, Realschule, Studienseminare
 
+
 class SaarlandSpider(CrawlSpider):
     name = "saarland"
-    # allowed_domains = ["www.saarland.de/4526.htm"]
     start_urls = ['https://www.saarland.de/mbk/DE/portale/bildungsserver/themen/schulen-und-bildungswege/schuldatenbank/_functions/Schulsuche_Formular.html']
-    
+
     rules = (Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="forward button"]',)), callback="parse_start_url", follow= True),)
-    
+
     def parse_start_url(self, response):
-        #schools = []
-        cards =  html.fromstring(response.text).xpath('//div[@class="c-teaser-card"]')
-        
+        cards = response.xpath('//div[@class="c-teaser-card"]')
+
         for card in cards:
-            card_copy = copy.deepcopy(card)           
-            c_badge = card_copy.xpath('//span[@class="c-badge"]/text()')
-            
             school = {}
-            school["name"] = card_copy.xpath('//h3/text()')[0].strip("\n").strip()
+            school["name"] = card.xpath('.//h3/text()').extract_first().strip()
+
+            c_badge = card.xpath('.//span[@class="c-badge"]/text()').extract()
             school["schultyp"] = c_badge[0]
             school["ort"] = c_badge[1]
-            
-            adress = card_copy.xpath('//p/text()')[0].split(", ")
-            
+
+            adress = card.xpath('.//p/text()').extract_first().split(", ")
+
             school["straße"] = adress[0]
             school["plz"] = adress[1].strip(" " + school['ort'])
-            
-            keys = card_copy.xpath('//dt/text()')
-            info = card_copy.xpath('//dd/text()')
-            
+
+            keys = card.xpath('.//dt/text()').extract()
+            info = card.xpath('.//dd/text()').extract()
+
             for index in range(0, len(keys)):
                 key = keys[index].strip(":").lower()
-                
-                if key == "homepage" :
-                    school["homepage"] = card_copy.xpath('//a[@target="_blank"]/text()')[0]
 
-                if key == "e-mail" : 
-                    school["e-mail"] = card_copy.xpath('//a[contains(@title, "E-Mail senden an:")]/@href')[0].strip("mailto:")
+                if key == "homepage" :
+                    school["homepage"] = card.xpath('.//a[@target="_blank"]/text()').extract_first()
+
+                if key == "e-mail" :
+                    school["e-mail"] = card.xpath('.//a[contains(@title, "E-Mail senden an:")]/@href').extract_first().strip("mailto:")
 
                 if key != "homepage" and key != "e-mail" :
                     school[key] = info[index]
-            
-            #schools.append(school)
+
             yield school
 
     @staticmethod

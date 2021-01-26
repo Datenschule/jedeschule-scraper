844a0fd70a2706e8b3c772bbc59fb152742a2c7f
Knut Hühne <knut@k-nut.eu>
2021-01-26 19:25:54 +0100

    Only log at level `info` in `scrape_all`

--- scrape_all.sh
+++ scrape_all.sh
@@ -8,5 +8,5 @@ do
   # cannot even use in the end.
   # This is why we set the maximum amount of items that are
   # allowed to crash to 3 here.
-  scrapy crawl $spider -s "CLOSESPIDER_ERRORCOUNT=3"
+  scrapy crawl $spider -s "CLOSESPIDER_ERRORCOUNT=3" --loglevel=INFO
 done

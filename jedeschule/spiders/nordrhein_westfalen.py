ca3c25972019af7d1826142bc6d11439f1f8b808
Knut Hühne <knut@k-nut.eu>
2021-02-14 22:11:12 +0100

    Store geolocation with schools

--- jedeschule/spiders/nordrhein_westfalen.py
+++ jedeschule/spiders/nordrhein_westfalen.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 
 from scrapy import Item
+from pyproj import Proj, transform
 
 from jedeschule.spiders.nordrhein_westfalen_helper import NordRheinWestfalenHelper
 from jedeschule.spiders.school_spider import SchoolSpider
@@ -20,10 +21,6 @@ class NordrheinWestfalenSpider(SchoolSpider):
     ]
 
     def parse(self, response):
-        # TODO: This data provider actually provides coordinate values that
-        # we can use at a later point. Extract them from here so that we don't
-        # have to fall back to geocoding
-
         body = response.body.decode('utf-8').splitlines()
         # skip the first line which contains information about the separator
         reader = DictReader(body[1:], delimiter=';')
@@ -36,6 +33,11 @@ class NordrheinWestfalenSpider(SchoolSpider):
                          item.get("Schulbezeichnung_2", ""),
                          item.get("Schulbezeichnung_3", "")]).strip()
         helper = NordRheinWestfalenHelper()
+        right, high = item.get('UTMRechtswert'), item.get('UTMHochwert')
+        this_projection = Proj(item.get('EPSG'))
+        target_projection = Proj('epsg:4326')
+        lon, lat = transform(this_projection, target_projection, right, high)
+
         return School(name=name,
                       id='NW-{}'.format(item.get('Schulnummer')),
                       address=item.get('Strasse'),
@@ -47,4 +49,7 @@ class NordrheinWestfalenSpider(SchoolSpider):
                       school_type=helper.resolve('schulform', item.get('Schulform')),
                       provider=helper.resolve('provider', item.get('Traegernummer')),
                       fax=f"{item.get('Faxvorwahl')}{item.get('Fax')}",
-                      phone=f"{item.get('Telefonvorwahl')}{item.get('Telefon')}")
+                      phone=f"{item.get('Telefonvorwahl')}{item.get('Telefon')}",
+                      latitude=lat,
+                      longitude=lon,
+                      )

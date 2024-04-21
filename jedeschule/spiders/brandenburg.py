15b4c98e030c4d444fa9722ed5cc15c4b4a672a8
Knut Hühne <knut@k-nut.eu>
2024-04-21 17:46:13 +0200

    [BB] Use WFS to get data

--- jedeschule/spiders/brandenburg.py
+++ jedeschule/spiders/brandenburg.py
@@ -1,60 +1,39 @@
-from typing import List, Optional
+import xml.etree.ElementTree as ET
 
-import scrapy
 from scrapy import Item
 
 from jedeschule.items import School
 from jedeschule.spiders.school_spider import SchoolSpider
 
 
-def first_or_none(item: List) -> Optional[str]:
-    return next(iter(item or []), None)
-
-
 class BrandenburgSpider(SchoolSpider):
     name = "brandenburg"
-    start_urls = ['https://bildung-brandenburg.de/schulportraets/index.php?id=uebersicht']
+
+    start_urls = [
+        "https://schullandschaft.brandenburg.de/edugis/wfs/schulen?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&typename=ms:Schul_Standorte"
+    ]
 
     def parse(self, response):
-        for link in response.xpath('/html/body/div/div[5]/div[2]/div/div[2]/table/tbody/tr/td/a/@href').getall():
-            yield scrapy.Request(response.urljoin(link), callback=self.parse_details)
+        elem = ET.fromstring(response.body)
 
-    def parse_details(self, response):
-        table = response.xpath('//*[@id="c"]/div/table')
-        data = {
-            # extract the school ID from the URL
-            'id': response.url.rsplit('=', 1)[1],
-            'data_url': response.url
-        }
-        for tr in table.css('tr:not(:first-child)'):
-            key = tr.css('th ::text').get().replace(':', '').strip()
-            value = tr.css('td ::text').getall()
-            data[key] = [self.fix_data(part) for part in value]
-        yield data
-
-    def fix_data(self, string):
-        """
-        fix wrong tabs, spaces and backslashes
-        fix @ in email addresses
-        """
-        if string is None:
-            return None
-        string = ' '.join(string.split())
-        return string.replace('\\', '').replace('|at|','@').strip()
+        for member in elem:
+            data_elem = {}
+            for attr in member[0]:
+                data_elem[attr.tag.split("}", 1)[1]] = attr.text
+            yield data_elem
 
     @staticmethod
     def normalize(item: Item) -> School:
-        *name, street, place = item.get('Adresse')
-        zip_code, *city_parts = place.split(" ")
-        return School(name=' '.join(name),
-                        id='BB-{}'.format(item.get('id')),
-                        address=street,
-                        zip=zip_code,
-                        city=' '.join(city_parts),
-                        website=first_or_none(item.get('Internet')),
-                        email=first_or_none(item.get('E-Mail')),
-                        school_type=first_or_none(item.get('Schulform')),
-                        provider=first_or_none(item.get('Schulamt')),
-                        fax=first_or_none(item.get('Fax')),
-                        phone=first_or_none(item.get('Telefon')),
-                        director=first_or_none(item.get('Schulleiter/in')))
+        return School(
+            name=item.get("schulname"),
+            id="BB-{}".format(item.get("schul_nr")),
+            address=item.get("strasse_hausnr"),
+            address2="",
+            zip=item.get("plz"),
+            city=item.get("ort"),
+            website=item.get("homepage"),
+            email=item.get("dienst_email"),
+            school_type=item.get("schulform"),
+            fax=item.get("faxnummer"),
+            phone=item.get("telefonnummer"),
+        )

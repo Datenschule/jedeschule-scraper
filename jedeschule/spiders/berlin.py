238a2a9f961df4d1c96ae6dc991024c17d78f57b
simonmandeleyl <simon.mandeleyl@friendsurance.de>
2025-05-27 12:22:26 +0200

    Reworked the berlin spider

--- jedeschule/spiders/berlin.py
+++ jedeschule/spiders/berlin.py
@@ -1,42 +1,33 @@
-import xml.etree.ElementTree as ET
+import json
+import logging
 
-from jedeschule.items import School
 from scrapy import Item
 
+from jedeschule.items import School
 from jedeschule.spiders.school_spider import SchoolSpider
 
 
 class BerlinSpider(SchoolSpider):
     name = "berlin"
     start_urls = [
-        "https://gdi.berlin.de/services/wfs/schulen?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&srsname=EPSG:4326&typename=fis:schulen"
+        "https://gdi.berlin.de/services/wfs/schulen?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&srsname=EPSG:4326&"
+        "typename=fis:schulen&outputFormat=application/json"
     ]
 
-    def parse(self, response):
-        tree = ET.fromstring(response.body)
+    def parse(self, response, **kwargs):
+        geojson = json.loads(response.text)
 
-        namespaces = {
-            "gml": "http://www.opengis.net/gml",
-            "fis": "http://www.berlin.de/broker",
-        }
-        for school in tree.find("gml:featureMembers", namespaces).findall(
-            "{schulen}schulen", namespaces
-        ):
-            data_elem = {}
-            for entry in school:
-                if entry.tag == "{http://www.opengis.net/gml}boundedBy":
-                    continue
-                if entry.tag == "{schulen}geom":
-                    # This nested entry contains the coordinates that we would like to expand
-                    lon, lat = entry.findtext(
-                        "gml:Point/gml:pos", namespaces=namespaces
-                    ).split(" ")
-                    data_elem["lat"] = lat
-                    data_elem["lon"] = lon
-                    continue
-                # strip the namespace before returning
-                data_elem[entry.tag.split("}", 1)[1]] = entry.text
-            yield data_elem
+        for feature in geojson.get("features", []):
+            properties = feature.get("properties", {})
+            coords = feature.get("geometry", {}).get("coordinates", [])
+
+            try:
+                properties["lon"] = str(coords[0])
+                properties["lat"] = str(coords[1])
+            except (TypeError, IndexError):
+                logging.warning("Skipping feature with invalid geometry")
+
+            yield properties
 
     @staticmethod
     def normalize(item: Item) -> School:

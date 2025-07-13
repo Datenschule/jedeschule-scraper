3208f3955020a3c05cd77944f5aafb381afff61c
simonmand <simonmay4@icloud.com>
2025-07-13 19:41:50 +0200

    Reworked hamburg spider + test

--- jedeschule/wfs_basic_parsers.py
+++ jedeschule/wfs_basic_parsers.py
@@ -3,15 +3,19 @@ import json
 from scrapy.http import Response
 
 
-def parse_geojson_features(response: Response):
+def parse_geojson_features(response: Response, invert: bool = False):
     geojson = json.loads(response.text)
 
     for feature in geojson.get("features", []):
         properties = feature.get("properties", {})
         coords = feature.get("geometry", {}).get("coordinates", [])
 
-        properties["lon"] = coords[0]
-        properties["lat"] = coords[1]
+        if not invert:
+            properties["lon"] = coords[0]
+            properties["lat"] = coords[1]
+        else:
+            properties["lat"] = coords[0]
+            properties["lon"] = coords[1]
 
 
         yield properties
---
+++ test/test_hamburg.py
@@ -0,0 +1,96 @@
+import unittest
+
+from scrapy.http import TextResponse
+
+from jedeschule.spiders.hamburg import HamburgSpider
+
+
+class TestHamburgSpider(unittest.TestCase):
+    def test_parse(self):
+        json_response = """
+        {
+            "type": "FeatureCollection",
+            "numberReturned": 1,
+            "numberMatched": 453,
+            "timeStamp": "2025-07-13T16:55:57Z",
+            "crs": "http://www.opengis.net/def/crs/EPSG/0/4326",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            53.601522503676144,
+                            10.047106063058099
+                        ]
+                    },
+                    "properties": {
+                        "abschluss": "Allgemeine Hochschulreife|erster allgemeinbildender Schulabschluss|Erweiterter erster allgemeinbildender Schulabschluss|mittlerer Schulabschluss|schulischer Teil der Fachhochschulreife",
+                        "adresse_ort": "22307 Hamburg",
+                        "adresse_strasse_hausnr": "Benzenbergweg 2",
+                        "ansprechp_klasse_5": "Nadine Kalsow",
+                        "ansprechp_buero": "Janka Gierck",
+                        "anzahl_schueler": 996,
+                        "anzahl_schueler_gesamt": "1261 an 2 Standorten",
+                        "bezirk": "Hamburg-Nord",
+                        "fax": "+49 40 428 88 15 22",
+                        "fremdsprache": "Englisch|Französisch|Spanisch|Spanisch",
+                        "fremdsprache_mit_klasse": "Englisch ab Klasse  5|Französisch ab Klasse  7|Spanisch ab Klasse  11|Spanisch ab Klasse  7",
+                        "ganztagsform": "GTS teilweise gebunden",
+                        "is_rebbz": "true",
+                        "kapitelbezeichnung": "Stadtteilschulen",
+                        "lgv_standortk_erwachsenenbildung": "No",
+                        "name_schulleiter": "Bianca Thies",
+                        "name_stellv_schulleiter": "Christian Pape",
+                        "name_oberstufenkoordinator": "Frau Scheuermann-Andersen *49 40 428 88 15-61",
+                        "name_verwaltungsleitung": "Grit Sobottka",
+                        "rebbz_homepage": "http://rebbz-winterhude.hamburg.de/",
+                        "rechtsform": "staatlich",
+                        "schueleranzahl_schuljahr": "2024",
+                        "schul_email": "stadtteilschule-helmuth-huebener@bsb.hamburg.de",
+                        "schul_homepage": "https://helmuthhuebener.de",
+                        "schul_id": "5043-0",
+                        "schul_telefonnr": "+49 40 428 88 15 0",
+                        "schulaufsicht": "Christine Zopff",
+                        "schulform": "Stadtteilschule",
+                        "schulinspektion_link": "https://www.hamburg.de/politik-und-verwaltung/behoerden/schulbehoerde/themen/schulaufsicht/inspektionsberichte/weiterfuehrende-schulen-hamburg-nord",
+                        "schulname": "Stadtteilschule Helmuth Hübener",
+                        "schultyp": "Hauptstandort",
+                        "sozialindex": "Stufe 2",
+                        "stadtteil": "Barmbek-Nord",
+                        "standort_id": "431",
+                        "zuegigkeit_kl_5": "7",
+                        "zustaendiges_rebbz": "ReBBZ Winterhude"
+                    },
+                    "id": 874864
+                }
+            ],
+            "links": []
+        }
+        """
+
+        spider = HamburgSpider()
+        response = TextResponse(
+            url="http://test_webserver.com",
+            body=json_response.encode("utf-8"),
+            encoding="utf-8",
+        )
+
+        schools = list(spider.parse(response))
+        self.assertEqual(len(schools), 1)
+
+        school = schools[0]
+        self.assertAlmostEqual(school["lon"], 10.047106063058099)
+        self.assertAlmostEqual(school["lat"], 53.601522503676144)
+        self.assertEqual(school["schul_id"], "5043-0")
+        self.assertEqual(school["schulname"], "Stadtteilschule Helmuth Hübener")
+        self.assertEqual(school["adresse_ort"], "22307 Hamburg")
+        self.assertEqual(school["adresse_strasse_hausnr"], "Benzenbergweg 2")
+        self.assertEqual(school["schul_telefonnr"], "+49 40 428 88 15 0")
+        self.assertEqual(school["fax"], "+49 40 428 88 15 22")
+        self.assertEqual(school["schul_email"], "stadtteilschule-helmuth-huebener@bsb.hamburg.de")
+        self.assertEqual(school["schul_homepage"], "https://helmuthhuebener.de")
+
+
+if __name__ == "__main__":
+    unittest.main()

27bd9bf05d44f148fbfc17e0ee0c383c61c63878
Matthias Jacob <dev@matthiasjacob.net>
2026-09-22 03:03:14 +0200

    Use richer Bayern Kultusministerium school data
    Switch the Bayern scraper from the WFS school-location feed to the public Kultusministerium school detail pages. The new source exposes the official Schulnummer, which lets us build stable BY-{school_number} IDs instead of relying on technical WFS feature UUIDs.
    
    The KM pages also provide substantially richer school-level data in one place: contact details, administrative metadata, legal status, BayernAtlas coordinates, current school-year statistics, care offers, and education tracks. This moves the scraper toward school identities rather than location features while keeping the requested number in the raw item for discovery checks.
    
    Enumerate the confirmed four-digit detail-page number space from 0001 upward and require each parsed page to expose its own Schulnummer. Update the Bayern tests to cover KM detail-page requests, enumeration, and normalization of the richer fields.
    
    Test: uv run python -m unittest test.test_bayern

--- test/test_bayern.py
+++ test/test_bayern.py
@@ -6,48 +6,95 @@ from jedeschule.spiders.bayern import BayernSpider
 
 
 class TestBayernSpider(unittest.TestCase):
-    def test_parse(self):
-        xml_response = """<?xml version='1.0' encoding='UTF-8'?>
-                <wfs:FeatureCollection xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                       xsi:schemaLocation="http://www.opengis.net/wfs/2.0"
-                                       xmlns:wfs="http://www.opengis.net/wfs/2.0" timeStamp="2025-08-11T09:35:15Z"
-                                       xmlns:gml="http://www.opengis.net/gml/3.2" numberMatched="unknown" numberReturned="0">
-                    <wfs:member>
-                        <schul:SchulstandorteFoerderzentren xmlns:schul="http://gdi.bayern/brbschul"
-                                                            gml:id="SCHUL_SCHULSTANDORTEFOERDERZENTREN_3721b800-751d-49a1-a6d2-19d237e7bcc8">
-                            <schul:schulname>Bayerische Landesschule</schul:schulname>
-                            <schul:strasse>Kurzstr. 2</schul:strasse>
-                            <schul:postleitzahl>81547</schul:postleitzahl>
-                            <schul:ort>München</schul:ort>
-                            <schul:schulart>Förderzentren</schul:schulart>
-                            <schul:geometry>
-                                <gml:Point
-                                        gml:id="SCHUL_SCHULSTANDORTEFOERDERZENTREN_3721b800-751d-49a1-a6d2-19d237e7bcc8_SCHUL_GEOMETRY"
-                                        srsName="EPSG:4326">
-                                    <gml:pos>11.5686076923 48.1047906989</gml:pos>
-                                </gml:Point>
-                            </schul:geometry>
-                        </schul:SchulstandorteFoerderzentren>
-                    </wfs:member>
-                </wfs:FeatureCollection>
-            """
+    def test_start_requests_use_km_detail_pages(self):
+        spider = BayernSpider(school_numbers="1216,2448")
 
+        requests = list(spider.start_requests())
+
+        self.assertEqual(len(requests), 2)
+        self.assertEqual(requests[0].url, "https://www.km.bayern.de/schule/1216")
+        self.assertEqual(requests[0].callback, spider.parse_school)
+
+    def test_enumerates_four_digit_candidate_numbers_by_default(self):
+        spider = BayernSpider(end_number=3)
+
+        requests = list(spider.start_requests())
+
+        self.assertEqual(
+            [request.url for request in requests],
+            [
+                "https://www.km.bayern.de/schule/0001",
+                "https://www.km.bayern.de/schule/0002",
+                "https://www.km.bayern.de/schule/0003",
+            ],
+        )
+
+    def test_parse_school(self):
+        html_response = """
+            <html>
+                <body>
+                    <h1>Grundschule Aßling</h1>
+                    <div class="articleLeft schoolSearchResult">
+                        <section>
+                            <div class="rxModule">
+                                <h2 class="rxFirstText">Kontakt</h2>
+                                <p>Schulstraße 3 - 5<br>85617 Aßling</p>
+                                <p>Telefon: 08092/4911<br>Fax: 08092/32806<br>Web:
+                                    <a class="website" href="https://www.schule-assling.de" target="_blank">www.schule-assling.de</a>
+                                </p>
+                                <a class="bigButton" href="https://geoportal.bayern.de/bayernatlas/?lang=de&amp;topic=ba&amp;bgLayer=atkis&amp;crosshair=marker&amp;E=12.0048456&amp;N=47.9903501&amp;zoom=12&amp;catalogNodes=11,122">Standort anzeigen (BayernAtlas)</a>
+                                <h2>Verwaltungsangaben</h2>
+                                <p>Schulnummer: 2448<br>Schulart: Grund- u. Mittel-/Hauptschulen<br>Rechtlicher Status: öffentliche Schule (staatlich)</p>
+                                <h2>Eckdaten im Schuljahr 2025/26</h2>
+                                <p>Vollzeit- und überhälftig teilzeitbeschäftigte Lehrkräfte: 012<br>Schüler: 0259</p>
+                                <h2>Besondere Betreuungsangebote</h2>
+                                <p>Deutsch in (ganzjährigen) Vorkursen<br>Mittagsbetreuung o.ä.</p>
+                                <h2>Ausbildungsrichtungen</h2>
+                                <p>Grundschule (Jahrgangsstufen 1-4, voll ausgebaut)</p>
+                            </div>
+                        </section>
+                    </div>
+                </body>
+            </html>
+            """
         spider = BayernSpider()
-        response = TextResponse(url="https://test.com", body=xml_response, encoding="utf-8")
-        schools = list(spider.parse(response))
+        response = TextResponse(
+            url="https://www.km.bayern.de/schule/2448",
+            body=html_response,
+            encoding="utf-8",
+        )
+
+        schools = list(spider.parse_school(response))
+
         self.assertEqual(len(schools), 1)
 
         school = schools[0]
         parsed_school = spider.normalize(school)
 
-        self.assertEqual(parsed_school["id"], "BY-SCHUL_SCHULSTANDORTEFOERDERZENTREN_3721b800-751d-49a1-a6d2-19d237e7bcc8")
-        self.assertEqual(parsed_school["name"], "Bayerische Landesschule")
-        self.assertEqual(parsed_school["address"], "Kurzstr. 2")
-        self.assertEqual(parsed_school["city"], "München")
-        self.assertEqual(parsed_school["school_type"], "Förderzentren")
-        self.assertEqual(parsed_school["zip"], "81547")
-        self.assertEqual(parsed_school["latitude"], 48.1047906989)
-        self.assertEqual(parsed_school["longitude"], 11.5686076923)
+        self.assertEqual(school["schulnummer"], "2448")
+        self.assertEqual(school["schuljahr"], "2025/26")
+        self.assertEqual(school["lehrkraefte"], 12)
+        self.assertEqual(school["schueler"], 259)
+        self.assertEqual(
+            school["betreuungsangebote"],
+            ["Deutsch in (ganzjährigen) Vorkursen", "Mittagsbetreuung o.ä."],
+        )
+        self.assertEqual(
+            school["ausbildungsrichtungen"],
+            ["Grundschule (Jahrgangsstufen 1-4, voll ausgebaut)"],
+        )
+        self.assertEqual(parsed_school["id"], "BY-2448")
+        self.assertEqual(parsed_school["name"], "Grundschule Aßling")
+        self.assertEqual(parsed_school["address"], "Schulstraße 3 - 5")
+        self.assertEqual(parsed_school["city"], "Aßling")
+        self.assertEqual(parsed_school["school_type"], "Grund- u. Mittel-/Hauptschulen")
+        self.assertEqual(parsed_school["legal_status"], "öffentliche Schule (staatlich)")
+        self.assertEqual(parsed_school["zip"], "85617")
+        self.assertEqual(parsed_school["phone"], "08092/4911")
+        self.assertEqual(parsed_school["fax"], "08092/32806")
+        self.assertEqual(parsed_school["website"], "https://www.schule-assling.de")
+        self.assertEqual(parsed_school["latitude"], 47.9903501)
+        self.assertEqual(parsed_school["longitude"], 12.0048456)
 
 
 if __name__ == "__main__":
